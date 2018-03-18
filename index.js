9893d414307bcb7890ae3e8beb23b31d7bd12540
cuay <37254058+cuay@users.noreply.github.com>
2018-03-18 11:41:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const token = config.token;
 
 client.on('ready', () => {
     console.log('Samuke is ready for action!');
+
+    client.user.setStatus('Online');
+
+    client.user.setGame('Hey!', 'https://www.twitch.tv/winterlect');
 });
 
 client.on('message', async message=> {
@@ -19,19 +23,21 @@ client.on('message', async message=> {
 
     //A little command for checking your latency.
     if (message.content === '=ping') {
-        const m = await message.channel.send("Ping?");
-        m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+        const m = await message.channel.send("Pinging...");
+
+        message.channel.send({embed: {
+            title: "Pong!",
+            description:  `${m.createdTimestamp - message.createdTimestamp}ms.`,
+            color: 0x63b4d1
+
+        }})
+
+       
     }
 
     //Samuke DM's you with some formatting tips.
     if (message.content === `${prefix}formatting`) {
         message.author.send(`This is a simple Discord formatting guide. \n\n> Place 2 asterisks before and after a word/sentence/paragraph to make the selected text bold: **BOLD** \n\n> Put two underscores before and after a word/sentence/paragraph to make the selected text underlined: __UNDERLINE__ \n\n> Place BOTH 2 asterisks and 2 underscores to make the text both underlined AND bold: __**BOTH BOLDED AND UNDERLINED!**__`);
-    }
-
-    //Samuke reacts to this with the thonk emoji, only enabled on servers with the thonk emoji
-    if (message.content === `${prefix}thonk`) {
-        const thonk = message.guild.emojis.find('name', 'thonk');
-        message.react(thonk);
     }
 
     //Check server info
@@ -46,8 +52,49 @@ client.on('message', async message=> {
     
     //Samuke DM's you with some key commands.
     if(message.content === `${prefix}help`){
-        message.author.send(`Hey! I'm Samuke, a friendly bot used in fun servers!\nBasic commands include:\n\n> =userinfo\n\n> =server\n\n> =ping\n\n and more! Thanks for choosing Samuke!\n\n - Developed by cuay 寒気#3395`);
+        message.author.send(`**👋 | Hey there, <@${message.author.id}>! I'm Samuke, a friendly bot used in fun servers!\n\n📰 | Basic commands include:\n\n✅ | =userinfo\n\n✅ | =server\n\n✅ | =ping\n\n and more! \n\n __Please edit this message if you are an administrator.__ \n\n 👍 | Thanks for choosing Samuke!\n\n - 🖥 | Developed by cuay 寒気#3395**`);
     }
+
+    if(message.content === `${prefix}avatar`){
+        message.reply(`here's your avatar:` + message.author.avatarURL);
+    }
+
+    if(message.content === `${prefix}credits`){
+        message.channel.send(`This incredible bot was made possible by:\n\nSignaturedX - Lead director and support\n\nSkaiz - Coding manager\n\ncuay - Graphic design and coding.`);
+    }
+
+    if(message.content.toLowerCase() === `ur mum gay`){
+        message.channel.send(`no u`);
+    }
+
+    if (message.content.startsWith(`${prefix}kick`)){
+        //=kick @cuay Offensive Language
+
+        let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+        if(!kUser) message.channel.send("Can't find that user!");
+        let kReason = args.join(" ").slice(22);
+        if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Sorry, I don't have enough permission!");
+
+        let kickEmbed = new Discord.RichEmbed()
+        .setDescription("Kick")
+        .setColor("003459")
+        .addField("Kicked User", `${kUser} with ID ${kUser.id}`)
+        .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
+        .addField("Kicked in", message.channel)
+        .addField("Time", message.createdAt)
+        .addField("Reason", kReason);
+
+        let kickChannel = message.guild.channels.find(`name`, "kicked-test");
+        if(!kickChannel) return message.channel.send("Can't find the kicked players channel!");
+
+        message.guild.member(kUser).kick(kReason);
+        kickChannel.send(kickEmbed);
+
+        return;
+    
+    }
+    
+
 
     //Individual commands, like "hello" and forcing Samuke to say something.
     if (!message.content.startsWith(prefix)) return;
@@ -61,6 +108,7 @@ client.on('message', async message=> {
         message.channel.send(sayMessage);
       }
     });
+    
 
     //A welcome message.
     client.on('guildMemberAdd', member => {
@@ -70,7 +118,19 @@ client.on('message', async message=> {
         if (!channel) return;
         // Send the message, mentioning the member
         channel.send(`Welcome to the server, ${member}! Please contact staff if you require assistance.`);
+
+        const role = member.addRole(member.guild.roles.find("name", "Coder"));
+
+        if (!role) return;
+
+        role.member.guild.roles.find("name", "Coder");
       });
 
 
 client.login(token);
+
+
+
+    //Below is my attempt at the kicking command. Still doesn't work.
+
+    
